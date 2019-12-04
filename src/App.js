01ad452263a006318b856ed41591f43a486068f6
Charlie Winslow <cwinslow22@yahoo.com>
2019-12-04 12:34:57 -0600

    added SearchBar and Logs components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 
 const App = () => {
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
   });
-  return <div className="App">IT Logger</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
